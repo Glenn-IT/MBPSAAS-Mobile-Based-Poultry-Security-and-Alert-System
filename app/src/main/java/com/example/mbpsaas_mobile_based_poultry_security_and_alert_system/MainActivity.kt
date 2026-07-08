1c00f018b0bc0a51576d5da88380d105622010b1
Glenn-IT <glenard2308@gmail.com>
2026-07-08 22:57:40 +0800

    initialize MBPSAAS project with basic structure and configuration files

---
+++ app/src/main/java/com/example/mbpsaas_mobile_based_poultry_security_and_alert_system/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mbpsaas_mobile_based_poultry_security_and_alert_system
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mbpsaas_mobile_based_poultry_security_and_alert_system.ui.theme.MBPSAASMobileBasedPoultrySecurityandAlertSystemTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MBPSAASMobileBasedPoultrySecurityandAlertSystemTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MBPSAASMobileBasedPoultrySecurityandAlertSystemTheme {
+        Greeting("Android")
+    }
+}
